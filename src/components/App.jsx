0260dc8d175422c6c415d4350e567357198e5c41
Andrey Pakhomov <pahomeathome@gmail.com>
2022-08-06 18:16:44 +0300

    main files added

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import Container from './Container/Container';
+
+export const App = () => {
+  return <Container></Container>;
+};
---
+++ src/components/Container/ContainerStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const ContainerStyle = styled.div`
+  max-width: 1170px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  background: linear-gradient(#0066aa, white);
+  text-align: center;
+  padding-top: 20px;
+`;
+
+export default ContainerStyle;
