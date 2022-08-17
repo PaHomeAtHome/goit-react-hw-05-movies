2de1840b9e30606468f5d4c5ac3a56e712710291
Andrey Pakhomov <pahomeathome@gmail.com>
2022-08-18 00:23:30 +0300

    Error added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ const Movies = lazy(() => import('../pages/Movies'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('../pages/Cast'));
 const Reviews = lazy(() => import('../pages/Reviews'));
+const Error = lazy(() => import('../pages/Error'));
 
 export const App = () => {
   const [results, setResults] = useState([]);
@@ -32,6 +33,7 @@ export const App = () => {
             <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
+        <Route path="*" element={<Error />} />
       </Routes>
     </Container>
   );
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import { Title } from 'components/ResultList/ResultList.styled';
+
+const Error = () => {
+  return (
+    <>
+      <Title>⚠️ Invalid URL or something wrong ⚠️</Title>
+    </>
+  );
+};
+
+export default Error;
