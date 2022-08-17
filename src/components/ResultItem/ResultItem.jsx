0eec3b65744aa6cfbb9df359d5104384112fad14
Andrey Pakhomov <pahomeathome@gmail.com>
2022-08-17 19:09:06 +0300

    search added

--- src/components/ResultItem/ResultItem.jsx
+++ src/components/ResultItem/ResultItem.jsx
@@ -3,13 +3,13 @@ import { Title, Label, Link } from './ResultItem.styled';
 
 export const ResultsItem = ({ result }) => {
   const { title, name, poster_path } = result;
+  const image =
+    (poster_path && 'https://image.tmdb.org/t/p/w500' + poster_path) ||
+    `https://critics.io/img/movies/poster-placeholder.png`;
   return (
     <MovieCard>
       <Link to={`movies/${result.id}`}>
-        <img
-          src={'https://image.tmdb.org/t/p/w500' + poster_path}
-          alt={title || name}
-        />
+        <img src={image} alt={title || name} />
         <Label>
           <Title>{title || name}</Title>
         </Label>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,19 +1,47 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import {
   SearchButton,
   SearchForm,
   SearchInput,
 } from 'components/ResultItem/ResultItem.styled';
+import { findMovies } from 'services/API';
+import { ResultsList } from '../components/ResultList/ResultList';
 
 export const Movies = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    findMovies(query).then(response => {
+      const results = response.map(movie => movie);
+      setResults(results);
+    });
+  }, [query]);
+
+  const updateQueryString = query => {
+    const nextParams = query !== '' ? { query } : {};
+    setSearchParams(nextParams);
+  };
+
   return (
-    <main>
-      <SearchForm>
+    <>
+      <SearchForm
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(e);
+          updateQueryString(e.target[0].value.trim().toLowerCase());
+          findMovies(query);
+        }}
+      >
         <SearchInput></SearchInput>
         <SearchButton>Search</SearchButton>
       </SearchForm>
+      {results.length > 0 && <ResultsList results={results} />}
 
       <Outlet />
-    </main>
+    </>
   );
 };
